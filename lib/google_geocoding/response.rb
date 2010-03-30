07ee3b605ec44640a7ccfbd256eb7c311f0b21e3
Rodrigo Kochenburger <divoxx@gmail.com>
2010-03-29 17:56:58 -0700

    Change response to reflect new API

--- lib/google_geocoding/response.rb
+++ lib/google_geocoding/response.rb
@@ -3,72 +3,32 @@ module GoogleGeocoding
     # Creates a new response object from the given response body, expects to be a JSON string.
     #
     # @param [String]
-    def initialize(resp_body)
-      @data = JSON.parse(resp_body)
+    def initialize(io, result_class = nil)
+      @data         = JSON.parse(io.read)
+      @result_class = result_class || GoogleGeocoding.const_get(:Result)
     end
 
     # Return the status code included in the server response.
     #
-    # @return [Integer] 
-    # @see http://code.google.com/apis/maps/documentation/geocoding/index.html#StatusCodes
-    def status_code
-      Integer(@data["Status"]["code"])
+    # @return [Symbol]
+    # @see http://code.google.com/apis/maps/documentation/geocoding/#StatusCodes
+    def status
+      @status ||= @data["status"].downcase.to_sym
     end
 
     # Return whether response successfully resolved the geolocation
     def success?
-      status_code == 200
+      status == :ok
     end
 
     # Return whether response failed to resolved the geolocation
     def failure?
       !success?
     end
-
-    # Return the placemarks included in the response.
-    #
-    # @return [Array<Placemark>]
-    def placemarks
-      if failure?
-        raise Errors::ServiceError.build(status_code)
-      else
-        placemarks = []
-
-        @data["Placemark"].each do |placemark_data|
-          details               = placemark_data["AddressDetails"]
-          coordinates           = placemark_data["Point"]["coordinates"][0..1].reverse
-          accurracy             = Integer(details["Accuracy"])
-          placemark             = Placemark.new
-          placemark.accurracy   = accurracy
-          placemark.coordinates = coordinates
-
-          if country = details["Country"]
-            placemark.country_name = country["CountryName"],
-            placemark.country_code = country["CountryNameCode"]
-
-            if admarea = country["AdministrativeArea"]
-              placemark.region = admarea["AdministrativeAreaName"]
-              subadmarea       = admarea["SubAdministrativeArea"]                
-              locality         = subadmarea ? subadmarea["Locality"] : admarea["Locality"]
-
-              if locality
-                placemark.city = locality["LocalityName"]
-
-                if postal_code = locality["PostalCode"]
-                  placemark.postal_code = postal_code["PostalCodeNumber"]
-                end
-
-                if thoroughfare = locality["Thoroughfare"]
-                  placemark.street = thoroughfare["ThoroughfareName"]
-                end
-              end
-            end
-          end
-
-          placemarks << placemark
-        end
-        placemarks
-      end
+    
+    # Return the types of the result
+    def results
+      @result ||= @data["results"].map { |result| @result_class.new(result) }
     end
   end
 end
---
+++ spec/response_spec.rb
@@ -0,0 +1,79 @@
+require File.expand_path("../spec_helper", __FILE__)
+
+describe GoogleGeocoding::Response do
+  before :each do
+    @result_inst  = mock(:result_instance)
+    @result_class = mock(:result_class, :new => @result_inst)
+    @response     = GoogleGeocoding::Response.new(StringIO.new(DATA), @result_class)
+  end
+  
+  it "should provide status" do
+    @response.status.should == :ok
+  end
+  
+  it "should provide whether it is a successful request" do
+    @response.should be_success
+  end
+  
+  it "should provide whether it is a failure request" do
+    @response.should_not be_failure
+  end
+  
+  it "should provide the results" do
+    @result_class.should_receive(:new).once.with(an_instance_of(Hash)).and_return(@result_inst)
+    @response.results.should be_instance_of(Array)
+    @response.results.each { |item| item.should be(@result_inst) }
+  end
+  
+  DATA = <<-EOF
+{
+  "status": "OK",
+  "results": [ {
+    "types": [ "street_address" ],
+    "formatted_address": "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
+    "address_components": [ {
+      "long_name": "1600",
+      "short_name": "1600",
+      "types": [ "street_number" ]
+    }, {
+      "long_name": "Amphitheatre Pkwy",
+      "short_name": "Amphitheatre Pkwy",
+      "types": [ "route" ]
+    }, {
+      "long_name": "Mountain View",
+      "short_name": "Mountain View",
+      "types": [ "locality", "political" ]
+    }, {
+      "long_name": "California",
+      "short_name": "CA",
+      "types": [ "administrative_area_level_1", "political" ]
+    }, {
+      "long_name": "United States",
+      "short_name": "US",
+      "types": [ "country", "political" ]
+    }, {
+      "long_name": "94043",
+      "short_name": "94043",
+      "types": [ "postal_code" ]
+    } ],
+    "geometry": {
+      "location": {
+        "lat": 37.4219720,
+        "lng": -122.0841430
+      },
+      "location_type": "ROOFTOP",
+      "viewport": {
+        "southwest": {
+          "lat": 37.4188244,
+          "lng": -122.0872906
+        },
+        "northeast": {
+          "lat": 37.4251196,
+          "lng": -122.0809954
+        }
+      }
+    }
+  } ]
+}
+  EOF
+end
